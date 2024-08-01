3cbedc6fb6cd28a89c471ff4a7f241fd57ef5179
sigee-min <minshigee@gmail.com>
2024-08-02 01:30:20 +0900

    update: copy_config move to copy

--- init_container.py
+++ init_container.py
@@ -5,18 +5,24 @@ import requests
 def copy_configs():
     src_directory = "/data/config-tmp/"
     dest_directory = "/data/config/"
+
+    os.makedirs(dest_directory, exist_ok=True)
+
     files = os.listdir(src_directory)
 
     for file in files:
         src_path = os.path.join(src_directory, file)
         dest_path = os.path.join(dest_directory, file)
 
-        # Check if the destination file exists and remove it
-        if os.path.exists(dest_path):
-            os.remove(dest_path)
+        try:
+            if os.path.exists(dest_path):
+                os.remove(dest_path)
 
-        # Move the file from src to dest
-        shutil.move(src_path, dest_path)
+            shutil.copy2(src_path, dest_path)
+            print(f"Copied {file} to {dest_path}")
+
+        except OSError as e:
+            print(f"Error copying {file} from {src_path} to {dest_path}: {str(e)}")
 
     print(f"Copied configuration files from {src_directory} to {dest_directory}")
 
@@ -28,10 +34,8 @@ def install_plugins():
         print("No PLUGIN_URLS environment variable set.")
         return
 
-    # Ensure the plugins directory exists
     os.makedirs(plugin_directory, exist_ok=True)
 
-    # Download each plugin from the provided URLs
     for url in plugin_urls.split(','):
         try:
             response = requests.get(url)
